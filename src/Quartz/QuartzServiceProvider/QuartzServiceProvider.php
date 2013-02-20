683d1d808398181968005d681a46a21d57b298b0
pgoergler <paul@wayr.org>
2013-02-20 10:17:13 +0400

    Fix: replace $app['quartz'] by $app['orm']

--- src/Quartz/QuartzServiceProvider/QuartzServiceProvider.php
+++ src/Quartz/QuartzServiceProvider/QuartzServiceProvider.php
@@ -14,17 +14,13 @@ class QuartzServiceProvider implements \Silex\ServiceProviderInterface
 
     public function boot(Application $app)
     {
-        $app['quartz']->init($app['quartz.databases']); // to init database
+        $app['orm']->init($app['quartz.databases']); // to init database
     }
 
     public function register(Application $app)
     {
         $app['quartz.databases'] = array();
         
-        $app['quartz'] = $app->share(function() use (&$app)
-                {
-                    return \Quartz\Quartz::getInstance();
-                });
         $app['orm'] = $app->share(function() use (&$app)
                 {
                     return \Quartz\Quartz::getInstance();
